f8edd17dc1f2ecfa1962df222aad6653f85e4e88
Eve <evekose@gmail.com>
2022-09-22 14:01:18 +0300

    salary calculator

--- SalaryCalculator/Program.cs
+++ SalaryCalculator/Program.cs
@@ -6,54 +6,54 @@ namespace SalaryCalculator
     {
         static void Main(string[] args)
         {
-            int GrossSalary = 0; //brutopalk
+            int grossSalary = 0; //brutopalk
 
             Console.WriteLine("Please enter your gross salary: ");
-            GrossSalary = Convert.ToInt32(Console.ReadLine());
+            grossSalary = Convert.ToInt32(Console.ReadLine());
 
-            if (GrossSalary >= 500 && GrossSalary <= 1200) //aastatulu kuni 14400eur aastas; maksuvaba 6000 aastas
+            if (grossSalary >= 500 && grossSalary <= 1200) //aastatulu kuni 14400eur aastas; maksuvaba 6000 aastas
             {
-                Method1(GrossSalary);
+                SalaryIsBetween500And1200(grossSalary);
             }
-            if (GrossSalary >= 1201 && GrossSalary <= 2099) 
+            if (grossSalary >= 1201 && grossSalary <= 2099) 
                 // aastatulu 14400 kuni 25200; maksuvaba arvutamise valem = 500 - 0.55556 x (tulu - 1200)
             {
-                Method2(GrossSalary);
+                SalaryIsBetween1201And2099(grossSalary);
             }
-            if (GrossSalary >= 2100) //aastatulu üle 25200; maksuvaba tulu on 0
+            if (grossSalary >= 2100) //aastatulu üle 25200; maksuvaba tulu on 0
             {
-                Method3(GrossSalary);
+                SalaryIsAbove2100(grossSalary);
             }
 
 
                 
         }
-        static void Method1(double GrossSalary)
+        public static void SalaryIsBetween500And1200(double grossSalary)
         {
-            double MandatoryFundedPension = GrossSalary * 0.02; //kohustuslik kogumispension 2%
-            double UnemploymentInsurancePremium = GrossSalary * 0.016; //tööstuskindlustusmakse 1,6%
-            double IncomeTax = (GrossSalary - 500 - MandatoryFundedPension - UnemploymentInsurancePremium) * 0.2; //tulumaks 20%
-            double Method1 = GrossSalary - MandatoryFundedPension - UnemploymentInsurancePremium - IncomeTax;
-            Console.WriteLine("Your net salary is: " + Method1); //netopalk
+            double mandatoryFundedPension = grossSalary * 0.02; //kohustuslik kogumispension 2%
+            double unemploymentInsurancePremium = grossSalary * 0.016; //tööstuskindlustusmakse 1,6%
+            double incomeTax = (grossSalary - 500 - mandatoryFundedPension - unemploymentInsurancePremium) * 0.2; //tulumaks 20%
+            double netSalary = grossSalary - mandatoryFundedPension - unemploymentInsurancePremium - incomeTax;
+            Console.WriteLine($"Your net salary is {netSalary}"); //netopalk
         }
 
-        static void Method2(double GrossSalary)
+        public static void SalaryIsBetween1201And2099(double grossSalary)
         {
-            double MandatoryFundedPension = GrossSalary * 0.02;
-            double UnemploymentInsurancePremium = GrossSalary * 0.016;
-            double TaxFreeIncome = 500 - 0.55556 * (GrossSalary - 1200); // tulumaksuvabastus
-            double IncomeTax = (GrossSalary - MandatoryFundedPension - UnemploymentInsurancePremium - TaxFreeIncome) * 0.2;
-            double Method2 = GrossSalary - MandatoryFundedPension - UnemploymentInsurancePremium - IncomeTax;
-            Console.WriteLine("Your net salary is: " + Method2);
+            double mandatoryFundedPension = grossSalary * 0.02;
+            double unemploymentInsurancePremium = grossSalary * 0.016;
+            double taxFreeIncome = 500 - 0.55556 * (grossSalary - 1200); // tulumaksuvabastus
+            double incomeTax = (grossSalary - mandatoryFundedPension - unemploymentInsurancePremium - taxFreeIncome) * 0.2;
+            double netSalary = grossSalary - mandatoryFundedPension - unemploymentInsurancePremium - incomeTax;
+            Console.WriteLine($"Your net salary is {netSalary}");
         }
 
-        static void Method3(double GrossSalary)
+        public static void SalaryIsAbove2100(double grossSalary)
         {
-            double MandatoryFundedPension = GrossSalary * 0.02;
-            double UnemploymentInsurancePremium = GrossSalary * 0.016;
-            double IncomeTax = (GrossSalary - MandatoryFundedPension - UnemploymentInsurancePremium) * 0.2;
-            double Method3 = (GrossSalary - MandatoryFundedPension - UnemploymentInsurancePremium) - IncomeTax;
-            Console.WriteLine("Your net salary is: " + Method3);
+            double mandatoryFundedPension = grossSalary * 0.02;
+            double unemploymentInsurancePremium = grossSalary * 0.016;
+            double incomeTax = (grossSalary - mandatoryFundedPension - unemploymentInsurancePremium) * 0.2;
+            double netSalary = (grossSalary - mandatoryFundedPension - unemploymentInsurancePremium) - incomeTax;
+            Console.WriteLine($"Your net salary is {netSalary} ");
         }
     }
 }
